5a6cacd147330a93bd1ab7331b5656e258be8641
DESKTOP-L88LUBN\София <sofiya.fish02@mail.ru>
2025-06-19 12:34:40 +0300

    feat: add not found page, fix suspense

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,16 +1,23 @@
 import { Route, Routes } from 'react-router-dom';
 import { Suspense } from 'react';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'shared/ui/PageLoader/PageLoader';
 
 const AppRouter = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Routes key={location.pathname}>
-        {Object.values(routeConfig).map(({ path, element }) => (
-          <Route key={path} path={path} element={<div className="page-wrapper">{element}</div>} />
-        ))}
-      </Routes>
-    </Suspense>
+    <Routes key={location.pathname}>
+      {Object.values(routeConfig).map(({ path, element }) => (
+        <Route
+          key={path}
+          path={path}
+          element={
+            <Suspense key={path} fallback={<PageLoader />}>
+              <div className="page-wrapper">{element}</div>
+            </Suspense>
+          }
+        />
+      ))}
+    </Routes>
   );
 };
 export default AppRouter;
