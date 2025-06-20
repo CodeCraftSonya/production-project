12306e3effae1428c143cdd509ef14b71b87a74b
DESKTOP-L88LUBN\София <sofiya.fish02@mail.ru>
2025-06-20 18:26:52 +0300

    feat: add error boundary component

---
+++ src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
@@ -0,0 +1,39 @@
+import React, { ErrorInfo } from 'react';
+import { PageError } from 'widgets/PageError/ui/PageError';
+
+interface ErrorBoundaryProps {
+  children: React.ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, info: ErrorInfo) {
+    // You can also log the error to an error reporting service
+    console.log(error, info);
+  }
+
+  render() {
+    const { hasError } = this.state;
+    const { children } = this.props;
+    if (hasError) {
+      return <PageError />;
+    }
+
+    return children;
+  }
+}
+
+export default ErrorBoundary;
